d1d9507e0f8a47b4e9e79c67030154e010f231c5
varshini komuroju <61497458+varshini959@users.noreply.github.com>
2024-04-25 10:35:51 +0530

    Add files via upload

---
+++ Ride-Sharing/src/main/java/com/Centum/Ride/Sharing/RideSharingApplication.java
@@ -0,0 +1,13 @@
+package com.Centum.Ride.Sharing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RideSharingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RideSharingApplication.class, args);
+	}
+
+}
---
+++ Ride-Sharing/src/test/java/com/Centum/Ride/Sharing/RideSharingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Centum.Ride.Sharing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RideSharingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
